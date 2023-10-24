bb3b2d5f9243a7bfba4faa95d3ce5584a925004c
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2023-10-23 23:44:55 -0300

    Correccion pagina principal

--- frontend/servicios/pagina_principal.js
+++ frontend/servicios/pagina_principal.js
@@ -1,32 +1,23 @@
+const dotenv = require("dotenv");
+
 const getVideojuegos = async () => {
-
-    let page = Math.floor(Math.random() * 20) + 1; 
-    console.log(page);
-    const videojuegos = await fetch(`https://api.rawg.io/api/games?key=4c8396ebdb16407383b027c49039f3f8&page=${page}`,
-      {
-        method: "GET",
-      }
-    )
-    .then(res => res.json())
-    .then((data) => data.results)
-    return videojuegos;
-}
-
-const  getNuevos = async () => {
-
-
-}
-
-const getGeneros = async () => {
-
+  let page = Math.floor(Math.random() * 20) + 1;
+  console.log(page);
+  const videojuegos = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.KEY_API}&page=${page}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => data.results);
+  return videojuegos;
 };
 
-const getCoincidenciaNombre = async () => {
+const getNuevos = async () => {};
 
-};
+const getGeneros = async () => {};
 
+const getCoincidenciaNombre = async () => {};
 
-const getOferats = async () => {
-
-}
-
+const getOferats = async () => {};
