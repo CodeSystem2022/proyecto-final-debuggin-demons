5caaca9c71629d53a81a3529b9019df5fd1d62bb
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-02 00:00:45 -0300

    [ADD] Expresiones regulares de nombre y emal

--- frontend/src/registro.js
+++ frontend/src/registro.js
@@ -1,2 +1,6 @@
 //Expresiones regulares
+//Expresion regular para email
 ///^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+
+//Expresion para nombres y apellidos
+//^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$
