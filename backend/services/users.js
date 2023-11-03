2b2b32a98cf97b76693bd361e4a41918dfe09423
Braian <troncosobraianaxel@gmail.com>
2023-11-02 23:06:19 -0300

    [ADD] function - login - services - async

--- backend/services/users.js
+++ backend/services/users.js
@@ -2,3 +2,9 @@ const usersRepo = require("../repositories/user");
 const userModel = require("../models/user")
 const bcrypt = require("bcrypt");
 
+
+const login = async (body) => {
+  const data = await usersRepo.findByUsername(body.username);
+  if (!data) {
+    throw new Error("Ocurrio un error al intntar Logearse");
+  }
