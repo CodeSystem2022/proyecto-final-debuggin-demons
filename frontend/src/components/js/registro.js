8f3784e7b2244b9385bb7bb8c80e6bc53ea26f5e
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-04 00:27:22 -0300

    [CHG] Arreglando la ruta del archivo js

--- frontend/src/components/js/registro.js
+++ frontend/src/components/js/registro.js
@@ -1,4 +1,13 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
 
 
 //Expresiones regulares
@@ -8,18 +17,18 @@
 //Expresion para nombres y apellidos
 //^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$
 
-function checkPass() {
-    let valContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
+// function checkPass() {
+//     let valContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
     
 
-    let contraseña = document.querySelector('#contraseña').value;
-    let mensaje = document.querySelector('#mensaje');
+//     let contraseña = document.querySelector('#contraseña').value;
+//     let mensaje = document.querySelector('#mensaje');
 
-    if (valContraseña.test(contraseña)) {
-        mensaje.textContent = 'Contraseña correcta';
+//     if (valContraseña.test(contraseña)) {
+//         mensaje.textContent = 'Contraseña correcta';
 
-    }else {
-        mensaje.textContent = 'Contraseña incorrecta'
-    }
-}
+//     }else {
+//         mensaje.textContent = 'Contraseña incorrecta'
+//     }
+// }
 
