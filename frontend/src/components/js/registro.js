519951dc9f6b2c99cf2fd110dd677f48b546e91a
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2023-11-04 01:47:00 -0300

    [ADD] Validando telefono

--- frontend/src/components/js/registro.js
+++ frontend/src/components/js/registro.js
@@ -18,20 +18,7 @@ const expresiones = {
 //Expresion para nombres y apellidos
 //^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$
 
-// function checkPass() {
-//     let valContraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/;
-    
 
-//     let contraseña = document.querySelector('#contraseña').value;
-//     let mensaje = document.querySelector('#mensaje');
-
-//     if (valContraseña.test(contraseña)) {
-//         mensaje.textContent = 'Contraseña correcta';
-
-//     }else {
-//         mensaje.textContent = 'Contraseña incorrecta'
-//     }
-// }
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
@@ -44,7 +31,7 @@ const validarFormulario = (e) => {
         break;
 
         case "username":
-           
+            validarCampo(expresiones.username, e.target, 'username');
         break;
 
         case "tel":
