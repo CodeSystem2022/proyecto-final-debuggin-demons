925d7b052d37c5e182e26dc205b89d9da3f37b2e
Braian <troncosobraianaxel@gmail.com>
2023-11-04 23:53:24 -0300

    [ADD] login async method and try catch

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,3 +2,10 @@ const usuariosService = require("../services/users");
 
 const login = async (req, res, next) => {
   console.log(req.body);
+  try {
+    const user = await usuariosService.login(req.body);
+    res.status(200).json({"msg": "Usario logeado", user: user})
+  } catch (e) {
+    next(e);
+  }
+};
