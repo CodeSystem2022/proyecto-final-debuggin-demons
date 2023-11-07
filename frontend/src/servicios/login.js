36494497e89bcc064eace36e4732d828dc283d52
Braian <troncosobraianaxel@gmail.com>
2023-11-07 14:09:03 -0300

    [ADD] const login - frontend - try - catch

--- frontend/src/servicios/login.js
+++ frontend/src/servicios/login.js
@@ -1,4 +1,9 @@
 const  login= async () => {
     const inputUsername = document.querySelector("#input-username-login").value;
     const inputPassword = document.querySelector("#input-password-login").value;
- 
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
