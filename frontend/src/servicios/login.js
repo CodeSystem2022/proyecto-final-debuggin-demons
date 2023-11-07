97b4f513467ee1f31b2355998d6000e78847c5c8
Braian <troncosobraianaxel@gmail.com>
2023-11-07 14:08:33 -0300

    [ADD] const login async - frontend

---
+++ frontend/src/servicios/login.js
@@ -0,0 +1,4 @@
+const  login= async () => {
+    const inputUsername = document.querySelector("#input-username-login").value;
+    const inputPassword = document.querySelector("#input-password-login").value;
+ 
