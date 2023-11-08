d9dc08355a618c22f94feea00ba6e9fd6fbecb9a
Braian <troncosobraianaxel@gmail.com>
2023-11-08 00:20:58 -0300

    [ADD] routes - uses - auth - compra - metodos - users

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,3 +6,10 @@ const carritoRouter = require("./carrito");
 const compraRouter = require("./compras");
 const metodosDePagoRouter = require("./metodosDePago");
 const userRouter = require("./users");
+
+
+router.use('/auth', authRouter);
+router.use('/carrito', carritoRouter);
+router.use("/compras", compraRouter);
+router.use('/metodos-de-pago', metodosDePagoRouter);
+router.use('/user', userRouter);
