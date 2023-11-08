7070169182046c5230437bc4c65d66be3a31aec0
Braian <troncosobraianaxel@gmail.com>
2023-11-08 00:20:39 -0300

    [ADD] routes - auth - compra - metodos - users

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const authRouter = require("./auth");
+const carritoRouter = require("./carrito");
+const compraRouter = require("./compras");
+const metodosDePagoRouter = require("./metodosDePago");
+const userRouter = require("./users");
